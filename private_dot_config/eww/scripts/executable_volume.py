0514f3c7799d0324046f9b4db9caa52cf6deb184
hypperd <84094975+hypperd@users.noreply.github.com>
2022-07-11 12:18:42 -0300

    updated volume.py

--- private_dot_config/eww/scripts/executable_volume.py
+++ private_dot_config/eww/scripts/executable_volume.py
@@ -3,28 +3,18 @@
 import asyncio
 import signal
 import sys
-from contextlib import suppress
 import pulsectl_asyncio
-import fcntl
 import os
-
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
-volume_old=-1
-
-
-
-#fileno = sys.stdout.fileno()
-#fcntl.fcntl(fileno, fcntl.F_GETFL, os.O_NONBLOCK)
+from contextlib import suppress
 
 async def listen():
-
-    async with pulsectl_asyncio.PulseAsync('event-printer') as pulse:
+    async with pulsectl_asyncio.PulseAsync('eww') as pulse:
         default_sink = await get_default_sink(pulse)
         default_source = await get_default_source(pulse)
 
         await write_volume_source(pulse, default_source.name)
-        await write_volume(pulse, default_sink.name)
+        await write_volume_sink(pulse, default_sink.name)
+
         async for event in pulse.subscribe_events('all'):
 
             if event.t != 'change':
@@ -33,19 +23,17 @@ async def listen():
             match event.facility:
                 case 'sink_input':
                     default_sink = await get_default_sink(pulse)
-                    await write_volume(pulse, default_sink.name)
+                    await write_volume_sink(pulse, default_sink.name)
+                case 'source_output':
+                    default_source = await get_default_source(pulse)
+                    await write_volume_source(pulse, default_source.name)
                 case 'sink':
                     if event.index == default_sink.index:
-                        await write_volume(pulse, default_sink.name)
-                case 'source_output':
-                    #default_sink = await get_default_sink(pulse)
-                    default_source = await get_default_source(pulse)
-                    #await write_volume(pulse, default_sink.name)
-                    await write_volume_source(pulse, default_source.name)
+                        await write_volume_sink(pulse, default_sink.name)
                 case 'source':
                     if event.index == default_source.index:
                         await write_volume_source(pulse, default_source.name)
-                case default:
+                case _:
                     continue
 
 async def get_default_sink(pulse: pulsectl_asyncio.PulseAsync):
@@ -58,25 +46,26 @@ async def get_default_source(pulse: pulsectl_asyncio.PulseAsync):
     source = await pulse.get_source_by_name(server_info.default_source_name)
     return source
 
-async def write_volume(pulse, name):
+async def write_volume_sink(pulse, name):
     sink = await pulse.get_sink_by_name(name)
     volume = await pulse.volume_get_all_chans(sink)
     
-    global volume_old
-    if volume_old != volume:
-        volume_old = volume
+    global sink_volume_old
+    if sink_volume_old != volume:
+        sink_volume_old = volume
         sys.stdout.write(str(round(volume * 100)) + '\n')
         sys.stdout.flush()
 
 async def write_volume_source(pulse, name):
     sink = await pulse.get_source_by_name(name)
     volume = await pulse.volume_get_all_chans(sink)
-    file = os.open('/home/hypper/.config/eww/test',os.O_WRONLY | os.O_NONBLOCK)
-    os.write(file, str(round(volume * 100)).encode() + '\n'.encode())
-    #os.flush(file)
-    os.close(file)
-    #sys.stderr.write(str(round(volume * 100)) + '\n')
-    #sys.stderr.flush()
+    
+    global source_volume_old
+    if source_volume_old != volume:
+        source_volume_old = volume
+        with open(fifo, 'w') as file:
+            file.write(str(round(volume * 100)) + '\n')
+            file.flush()
 
 
 async def main():
@@ -91,6 +80,14 @@ async def main():
     with suppress(asyncio.CancelledError):
         await listen_task
 
+
+
+fifo = os.getenv('XDG_RUNTIME_DIR') + '/eww.pipe'
+if not os.path.exists(fifo):
+    os.mkfifo(fifo)
+
+source_volume_old = None
+sink_volume_old = None
 # Run event loop until main_task finishes
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
