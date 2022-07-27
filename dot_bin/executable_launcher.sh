6382a410310f3505004f54e73f14626f27ee8f46
hypperd <84094975+hypperd@users.noreply.github.com>
2022-07-27 19:23:17 -0300

    removed old rofi scripts

--- dot_bin/executable_launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "$(pgrep rofi)" ]; then
-    pkill rofi
-fi
-    swaymsg unbindsym '$mod+d'
-    rofi -show drun -theme ~/.config/rofi/themes/launcher.rasi -kb-cancel 'Super_L+d' -kb-cancel 'Escape'
-    swaymsg bindsym '$mod+d' exec '$bin/launcher.sh'
