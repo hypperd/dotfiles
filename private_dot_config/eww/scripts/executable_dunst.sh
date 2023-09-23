3e9c0ef25069d2cfa18c44e2d791337d78e0e392
hypperd <hypperdo@proton.me>
2023-09-23 17:03:24 -0300

    refactor: remove eww

--- private_dot_config/eww/scripts/executable_dunst.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-dunstctl set-paused toggle
-eww update dunst_paused="$(dunstctl is-paused)"
--- private_dot_config/eww/scripts/executable_night-light.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pidof -q gammastep; then
-    pkill gammastep
-    eww update night_light=false
-else
-    eww update night_light=true
-    swaymsg exec gammastep 
-fi
